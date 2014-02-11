5dc7f5cf9b14437cd3d2ab282b60e5d9646925aa
Satoshi Inoue <satoshiinoue.biz@gmail.com>
2014-02-10 23:54:57 -0500

    css style changed

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 //var messages = [];
 var socket = io.connect();
- 
+var numOfMsg = 0;
+var style = "";
 socket.on('connect', function () {
 	console.log('Socket connected');
 });
@@ -9,10 +10,16 @@ socket.on('message', function (data) {
 	console.log(data.message);
     if(data.message) {
         //messages.push(data);
+    	numOfMsg++;
+    	if (numOfMsg%2 != 0)
+    		style = "bgDark";
+    	else
+    		style = "bgLight";
+    	
         if (data.systemMsg) {
-        	$("#messages").append("<li>" + data.message + "</li>");
+        	$("#messages").append("<li class=\"" + style + "\">" + data.message + "</li>");
         } else {
-        	$("#messages").append("<li>" + data.username + " said: " + data.message + "</li>");
+        	$("#messages").append("<li class=\"" + style + "\">" + data.username + " said: " + data.message + "</li>");
         }
     } else {
         console.log("Problem occured:", data);
@@ -33,3 +40,8 @@ $("#send").click(function() {
         socket.emit('send', { message: $("#new-message").val(), username: $("#name").val()});
     }
 });
+
+window.onhashchange = function () {
+	console.log("onhashchange");
+  socket.send(window.location.href);
+}
